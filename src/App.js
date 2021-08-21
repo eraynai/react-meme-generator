dd3c0cc18c617f81014f86a3c06843a8eb73a4e7
Elli Raynai <elli@elliraynai.com>
2021-08-21 10:58:05 -0400

    added unordered list to App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import './App.css';
 
 function App() {
-	return <div className='App'></div>;
+	return (
+		<div className='App'>
+			<ul>
+				<li>Do Code Review</li>
+				<li>Practice Coding</li>
+				<li>Work Out for the First Time In A While</li>
+			</ul>
+		</div>
+	);
 }
 
 export default App;
